7e9ad96b30b3118b62391d3f605882de02b34188
LeehoLim <leeholim@uchciago.edu>
2017-01-25 21:06:56 -0600

    Completed community.cpp

--- src/community.cpp
+++ src/community.cpp
@@ -13,9 +13,10 @@ string Community::get_name() {
 }
 
 bool Community::set_name(string _name) {
-	//TODO
-	// set name
-    return false;
+	if((str_isalnum(_name) == 1) && (0 < _name.size()) && (_name.size() <= 128) && (std::isdigit(_name[0]) == 0)) {
+    return true;
+  }
+  return false;
 }
 
 bool Community::add_person(Person _person) {
@@ -36,32 +37,62 @@ Person Community::get_member(string username) {
     
 list<string> Community::get_all_usernames() {
     list<string> usernames;
-    // TODO
+    std::map<string,Person>::iterator it = people.begin();
+    while (it != people.end()) {
+      usernames.push_back(it->first);
+      ++it;
+    }
+
     // return all usernames of members as a list
     return usernames;
 }
 
 void Community::print_all_usernames() {
-    // TODO
+    std::map<string,Person>::iterator it = people.begin();
+    while (it != people.end()) {
+      cout << it->first;
+      ++it;
+    }
 	// print all usernames of members to the console
 }
 
 list<Person> Community::find_member(string firstname) {
     list<Person> ret;
-    //TODO
+    std::map<string,Person>::iterator it = people.begin();
+    while (it != people.end()) {
+      if(firstname.compare(it->second.get_firstname()) == 0) {
+        ret.push_back(it->second);
+        ++it;
+    }
     // find users with a certain first name
-    return ret;
+  }
+  return ret;
 }
 
 list<Person> Community::find_member(int age_lb, int age_ub) {
   list<Person> ret;
-  //TODO
+  std::map<string,Person>::iterator it = people.begin();
+  while (it != people.end()) {
+    int age = it->second.get_age();
+      if(age_lb <= age <= age_ub){
+        ret.push_back(it->second);
+      }
+    }
   // find users within certain ages [age_lb, age_ub], both lower bound and upper bound shall be inclusive
   return ret;
 }
 
 bool Community::send_msg(list<string> usernames, string msg) {
-	//TODO
+  std::list<string>::iterator it = usernames.begin();
+  while(it != usernames.end()) {
+    if(get_member(*it).get_info() == "") {
+      continue;
+    }
+    else {
+      get_member(*it).get_msg(msg);
+    }
+    ++it;
+  }
 	// send msg to a Person addressed by username
 	// make sure the username is validated
 	return false;
